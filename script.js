2db62c8924ad11cf7cbd74bd68b69e6618d88057
JavAlisson <javalisson@gmail.com>
2017-05-18 12:27:22 -0300

    Desenhadas as versões estáticas dos bastões e da bola.

--- script.js
+++ script.js
@@ -16,4 +16,13 @@ function draw() {
 	// pinta o fundo de azul escuro
 	background(19, 29, 140);
 
+	// bastao jogador 1
+	fill(255, 255, 255);
+	rect(23, 99, 32, 107); // posicao x, posicao y, largura, altura
+	// bastao jogador 2
+	fill(255, 255, 255);
+	rect(744, 129, 32, 107); // posicao x, posicao y, largura, altura
+	// bola
+	fill(113, 122, 236);
+	ellipse(385, 119, 35);
 }
