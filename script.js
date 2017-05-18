91df5e354789b49cd0474890aac17bbf40d4bdd5
JavAlisson <javalisson@gmail.com>
2017-05-18 12:07:19 -0300

    Criados os arquivos html e js para desenhar o quadro do jogo. Adicionada a biblioteca p5.js.

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * p5js pong
+ * 
+ * 
+ */
+
+// chamada no inicio do programa
+function setup() {
+	// cria o quadro, com dimensoes 800 x 400
+	createCanvas(800, 400);
+}
+
+// chamada toda vez que o quadro for redesenhado
+// ou seja, a cada frame da animacao
+function draw() {
+	// pinta o fundo de preto
+	background(0, 0, 0);
+
+}
